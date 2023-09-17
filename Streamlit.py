4edde36c87161cdffc6ece2c4d7c5ead5609b8ab
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2023-09-18 08:59:45 +0900

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -8,12 +8,13 @@ import pygame
 import torch
 
 # YOLOv5 모델 불러오기
-model_weights_path = './models/best_big_bounding.pt'
-model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_weights_path)
+model_weights_path = "./models/best_big_bounding.pt"
+model = torch.hub.load("ultralytics/yolov5", "custom", path=model_weights_path)
 model.eval()
 
 # 이전 알림 시간 기록
 previous_alert_time = 0
+
 
 # 각도 계산 함수
 def calculateAngle(a, b, c):
@@ -31,6 +32,7 @@ def calculateAngle(a, b, c):
 
     return angle
 
+
 # YOLOv5를 사용한 사물 검출 함수
 def detect_objects(frame):
     results = model(frame)
@@ -38,8 +40,21 @@ def detect_objects(frame):
 
     return pred
 
+
 # Streamlit 앱 초기화
 st.title("실시간 3대 운동 AI 자세 교정 서비스")
+
+# Sidebar에 메뉴 추가
+menu_selection = st.selectbox("운동 선택", ("벤치프레스", "스쿼트", "데드리프트"))
+
+# Load different models based on the selected exercise
+# if menu_selection == "벤치프레스":
+#     model_weights_path = './models/benchpress/benchpress.pkl'
+# elif menu_selection == "스쿼트":
+#     model_weights_path = './models/squat/squat.pkl'
+# elif menu_selection == "데드리프트":
+#     model_weights_path = './models/deadlift/deadlift.pkl'
+
 FRAME_WINDOW = st.image([])
 camera = cv2.VideoCapture(0)
 
@@ -75,7 +90,7 @@ while True:
                 c2 = (c2[0].item(), c2[1].item())
 
                 # YOLOv5로 검출된 객체의 프레임 추출
-                object_frame = frame[c1[1]:c2[1], c1[0]:c2[0]]
+                object_frame = frame[c1[1] : c2[1], c1[0] : c2[0]]
 
                 # Pose estimation을 수행하기 위해 객체 프레임을 처리
                 object_frame_rgb = cv2.cvtColor(object_frame, cv2.COLOR_BGR2RGB)
@@ -117,7 +132,6 @@ while True:
                     left_angle_display.text(f"Left Angle: {left_angle:.2f}")
                     right_angle_display.text(f"Right Angle: {right_angle:.2f}")
 
-
                     # 화면에 각도 표시
                     frame = cv2.putText(
                         frame,
@@ -144,9 +158,13 @@ while True:
                         current_time = time.time()
                         if current_time - previous_alert_time >= 3:
                             now = datetime.datetime.now()
-                            st.write(f"자세가 무너졌습니다! - {now.hour}시 {now.minute}분 {now.second}초")
+                            st.write(
+                                f"자세가 무너졌습니다! - {now.hour}시 {now.minute}분 {now.second}초"
+                            )
                             pygame.mixer.init()
-                            pygame.mixer.music.load("./resources/sounds/broken_posture.mp3")
+                            pygame.mixer.music.load(
+                                "./resources/sounds/broken_posture.mp3"
+                            )
                             pygame.mixer.music.play()
                             # 이전 알림 시간 갱신
                             previous_alert_time = current_time
@@ -162,11 +180,18 @@ while True:
                             )
 
                 # 객체 프레임을 원본 프레임에 다시 그립니다.
-                frame[c1[1]:c2[1], c1[0]:c2[0]] = object_frame
+                frame[c1[1] : c2[1], c1[0] : c2[0]] = object_frame
 
                 frame = cv2.rectangle(frame, c1, c2, (0, 255, 0), 2)
-                frame = cv2.putText(frame, label, (c1[0], c1[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                
+                frame = cv2.putText(
+                    frame,
+                    label,
+                    (c1[0], c1[1] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (0, 255, 0),
+                    2,
+                )
 
     # 원본 프레임을 출력
     FRAME_WINDOW.image(frame)
